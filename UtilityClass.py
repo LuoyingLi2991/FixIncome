c83a2813de01be01eb8433806e24d5b0536c7d52
Luoying <luoying.li@crescentam.com>
2017-09-08 12:26:28 +0800

    update z score, percentile and percentile level method

--- UtilityClass.py
+++ UtilityClass.py
@@ -1,22 +1,22 @@
 import numpy as np
 from scipy import stats
+import pandas as pd
+import datetime 
+
 
 class UtilityClass:
     """Calculate Z_score of a given time series"""
+    
     @staticmethod
-    def calc_z_score(series_list, symmetric):
-        """Calculate Z_score
-
-        Arguments:
-        series_list -- list of returns to be analysed
-        symmetric -- a boolean variable indicating whether the series is symmetric
-        """
+    def z_score(series_list,symmetric):
         mu = np.mean(series_list)
         sigma = np.std(series_list)
         if symmetric:
+            
             return (series_list[-1]-mu)/sigma
         # if the series is not symmetric, compute two different sigma
         else:
+            #print series_list[-1][0]
             below_aver = list(filter(lambda x: x < mu, series_list))
             above_aver = list(filter(lambda x: x > mu, series_list))
             sigma_neg = np.std(below_aver)
@@ -25,27 +25,152 @@ class UtilityClass:
                 return (series_list[-1]-mu)/sigma_pos
             else:
                 return (series_list[-1]-mu)/sigma_neg
+    
+    @staticmethod
+    def calc_z_score(df, symmetric, *choice):
+        """Calculate Z_score
+
+        Arguments:
+        df -- a dataframe to be analysed
+        symmetric -- a boolean variable indicating whether the series is symmetric
+        """
+        
+        zscore=[]
+        series_list=df[df.columns[0]].values
+        idx=df.index.tolist()
+        idx_now=idx[-1]
+        zscore.append(UtilityClass.z_score(series_list,symmetric))
+        
+        if choice is not None:
+            for each in choice:
+                if each=='1d':
+                    while True:
+                        lastD=idx_now-datetime.timedelta(days=1)
+                        if lastD in idx:
+                            break
+                    df1=df.loc[: lastD]
+                    series_list=df1[df1.columns[0]].values
+                    #print series_list
+                    zscore.append(UtilityClass.z_score(series_list,symmetric))
+                if each=='1w':
+                    lastW=idx_now-datetime.timedelta(weeks=1)
+                    while lastW not in idx:
+                        lastW=lastW-datetime.timedelta(days=1)
+                    df1=df.loc[: lastW]
+                    series_list=df1[df1.columns[0]].values
+                    zscore.append(UtilityClass.z_score(series_list,symmetric))
+                if each=='1m':
+                    lastM=idx_now-datetime.timedelta(days=30)
+                    while lastM not in idx:
+                        lastM=lastM-datetime.timedelta(days=1)
+                    df1=df.loc[: lastM]
+                    series_list=df1[df1.columns[0]].values
+                    zscore.append(UtilityClass.z_score(series_list,symmetric))
+        #print zscore
+        return zscore        
+                                               
 
     @staticmethod
-    def calc_percentile(series_list):
+    def calc_percentile(df, *choice):
         """return the percentile of the last point in the list
 
         Argument:
         series_list -- list of returns to be analysed
         """
-        return stats.percentileofscore(series_list,series_list[-1],kind='weak')
+        pctl=[]
+        series_list=df[df.columns[0]].values
+        #print series_list
+        idx=df.index.tolist()
+        idx_now=idx[-1]
+        #print series_list[-1]
+        pctl.append(stats.percentileofscore(series_list,series_list[-1],kind='weak'))
+        
+        if choice is not None:
+            for each in choice:
+                if each=='1d':
+                    while True:
+                        lastD=idx_now-datetime.timedelta(days=1)
+                        if lastD in idx:
+                            break
+                    df1=df.loc[: lastD]
+                    series_list=df1[df1.columns[0]].values
+                    pctl.append(stats.percentileofscore(series_list,series_list[-1],kind='weak'))
+                if each=='1w':
+                    lastW=idx_now-datetime.timedelta(weeks=1)
+                    while lastW not in idx:
+                        lastW=lastW-datetime.timedelta(days=1)
+                    df1=df.loc[: lastW]
+                    series_list=df1[df1.columns[0]].values
+                    pctl.append(stats.percentileofscore(series_list,series_list[-1],kind='weak'))
+                if each=='1m':
+                    lastM=idx_now-datetime.timedelta(days=30)
+                    while lastM not in idx:
+                        lastM=lastM-datetime.timedelta(days=1)
+                    df1=df.loc[: lastM]
+                    series_list=df1[df1.columns[0]].values
+                    pctl.append(stats.percentileofscore(series_list,series_list[-1],kind='weak'))      
+        return pctl
 
 
     @staticmethod
-    def calc_percentile_level(series_list, perctl):
+    def calc_percentile_level(df, perctl, *choice):
         """return the percentile level of a given list
 
         Argument:
         series_list -- list of returns to be analysed
         perctl -- target percentile e.g 25 means the method returns a value of 25% percentile of the list
         """
-        return np.percentile(series_list, perctl)
+        lvl=[]
+        series_list=df.values.tolist()
+        idx=df.index.tolist()
+        idx_now=idx[-1]
+        lvl.append(np.percentile(series_list, perctl))
+        
+        if choice is not None:
+            for each in choice:
+                if each=='1d':
+                    while True:
+                        lastD=idx_now-datetime.timedelta(days=1)
+                        if lastD in idx:
+                            break
+                    df1=df.loc[: lastD]
+                    series_list=df1.values.tolist()
+                    lvl.append(np.percentile(series_list, perctl))
+                if each=='1w':
+                    lastW=idx_now-datetime.timedelta(weeks=1)
+                    while lastW not in idx:
+                        lastW=lastW-datetime.timedelta(days=1)
+                    df1=df.loc[: lastW]
+                    series_list=df1.values.tolist()
+                    lvl.append(np.percentile(series_list, perctl))
+                if each=='1m':
+                    lastM=idx_now-datetime.timedelta(days=30)
+                    while lastM not in idx:
+                        lastM=lastM-datetime.timedelta(days=1)
+                    df1=df.loc[: lastM]
+                    series_list=df1.values.tolist()
+                    lvl.append(np.percentile(series_list, perctl))      
+        return lvl
+        
+'''
 
-
-
-
+if __name__ == "__main__":
+    today=datetime.date.today()
+    dates=[]
+    for i in range(100):
+        dd=today-datetime.timedelta(days=1+i)
+        dates.insert(0,dd)
+    
+    s = pd.Series(range(100), index=dates)
+    u=UtilityClass()
+    Result=[]
+    Result.append(u.calc_z_score(s,True))
+    Result.append( u.calc_z_score(s,True,'1d','1w'))
+    Result.append( u.calc_z_score(s,False))
+    Result.append( u.calc_z_score(s,False,'1d','1w'))
+    Result.append( u.calc_percentile(s))
+    Result.append( u.calc_percentile(s,'1d','1w'))
+    Result.append( u.calc_percentile_level(s,95))
+    Result.append( u.calc_percentile_level(s,95,'1d','1w'))
+    
+'''
