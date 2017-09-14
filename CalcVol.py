3ba54826b7683d79219fa636a6b9e7e4c1e46eec
Luoying <luoying.li@crescentam.com>
2017-09-14 13:52:46 +0800

    calc vol

---
+++ CalcVol.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 14 09:11:39 2017
+
+@author: luoying.li
+"""
+import numpy as np
+import InterfaceFs
+
+def calc_historic_vol(timeseries, frequency):
+    frequency_dict={'d':252,'w':52, 'm':12, 'a':1}
+    temp=[np.log(timeseries[x+1]/timeseries[x]) for x in range(len(timeseries)-1)]
+    sigma_temp = np.std(temp,ddof=1)
+    rlt=sigma_temp*np.sqrt(frequency_dict[frequency])
+    return rlt
+
+
+
+if __name__ == "__main__":
+    conn_str = ('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)}; '
+                'DBQ=C:\\Test.accdb;')
+    [crsr,cnxn]=InterfaceFs.Build_Access_Connect(conn_str)
+    df=InterfaceFs.Tables2DF(crsr,'Spot',LB='1y').values()[0]
+    s=df['6m'].tolist()
+    print calc_historic_vol(s,'d')
+    
+   
+    
