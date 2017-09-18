f65813f0e881e50ae805bb2998c012b4ba58b075
Luoying <luoying.li@crescentam.com>
2017-09-18 14:01:47 +0800

    calc vol as formula

--- CalcVol.py
+++ CalcVol.py
@@ -6,10 +6,12 @@ Created on Thu Sep 14 09:11:39 2017
 """
 import numpy as np
 import InterfaceFs
+import pandas as pd
+
 
 def calc_historic_vol(timeseries, frequency):
     frequency_dict={'d':252,'w':52, 'm':12, 'a':1}
-    temp=[np.log(timeseries[x+1]/timeseries[x]) for x in range(len(timeseries)-1)]
+    temp=[(timeseries[x+1]-timeseries[x])/timeseries[x] for x in range(len(timeseries)-1)]
     sigma_temp = np.std(temp,ddof=1)
     rlt=sigma_temp*np.sqrt(frequency_dict[frequency])
     return rlt
@@ -22,7 +24,21 @@ if __name__ == "__main__":
     [crsr,cnxn]=InterfaceFs.Build_Access_Connect(conn_str)
     df=InterfaceFs.Tables2DF(crsr,'Spot',LB='1y').values()[0]
     s=df['6m'].tolist()
-    print calc_historic_vol(s,'d')
+    print calc_historic_vol(s, 'd')
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
    
     
